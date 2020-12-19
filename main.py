d1a4081081fc88c45d76496cf67bce70bc680011
sincy <sincyraj86@gmail.com>
2020-12-19 09:28:40 +0100

    Adapting code to make Flask-SQLAlchemy work

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
 from datetime import datetime
-
-from sqlalchemy import MetaData
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
+from model.base import db
 from model.address import Address
-from model.base import Base
 from model.company import Company
 from model.internship import Internship
 from model.program import Program
 from model.student import Student
 from model.university import University
 
-engine = create_engine('mysql+pymysql://vub:vub1234!@127.0.0.1/vub', pool_recycle=3600)
-metadata = MetaData(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+# engine = create_engine('mysql+pymysql://vub:vub1234!@127.0.0.1/vub', pool_recycle=3600)
+# metadata = MetaData(engine)
+# db.session = db.sessionmaker(bind=engine)
+#db.session = db.session()
 
-Base.metadata.create_all(engine)
+db.create_all()
 address1 = Address("Distelstraat 6", "Schaerbeek", "Brussels", "Brussels", "Belgium", "1030")
 address2 = Address("Hemeryckxlaan 10", "Kontich", "Antwerp", "Antwerp", "Belgium", "2660")
 address3 = Address("Uitbreidingstraat 8", "Berchem", "Antwerp", "Antwerp", "Belgium", "2600")
@@ -35,19 +30,19 @@ university = University("ua_uni1", "password", "+32889966771", "admin@uni.be", "
 university.address = address3
 university.programs.append(program)
 
-session.add(address1)
-session.add(address2)
-session.add(company)
-session.add(program)
-session.add(student)
-session.add(university)
-session.commit()
+db.add(address1)
+db.add(address2)
+db.add(company)
+db.add(program)
+db.add(student)
+db.add(university)
+db.commit()
 
-db_student = session.query(Student).filter(Student.id == 2).one()
-db_university = session.query(University).filter(University.id == 3).one()
-db_company = session.query(Company).filter(Company.id == 1).one()
+db_student = db.session.query(Student).filter(Student.id == 2).one()
+db_university = db.session.query(University).filter(University.id == 3).one()
+db_company = db.session.query(Company).filter(Company.id == 1).one()
 internship = Internship("INT1", db_university.id, db_student.id, db_company.id, "AVAILABLE", datetime.today(),
                         datetime.today())
 
-session.add(internship)
-session.commit()
+db.session.add(internship)
+db.session.commit()
