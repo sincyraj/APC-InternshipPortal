10c9065746e1f0650904dc46a155a4993a69a8fc
sincy <sincyraj86@gmail.com>
2020-12-19 16:27:12 +0100

    Minor changes in the template code.

--- populate_database.py
+++ populate_database.py
@@ -13,17 +13,22 @@ db.create_all()
 address1 = Address("Distelstraat 6", "Schaerbeek", "Brussels", "Brussels", "Belgium", "1030")
 address2 = Address("Hemeryckxlaan 10", "Kontich", "Antwerp", "Antwerp", "Belgium", "2660")
 address3 = Address("Uitbreidingstraat 8", "Berchem", "Antwerp", "Antwerp", "Belgium", "2600")
+address4 = Address("Celestijnenlaan 69", "", "Leuven", "Vlaams-Brabant", "Belgium", "3000")
 
 company = Company("uza", "password", "+3212345678", "user@example.com", "UZA", "CPY1")
 company.address = address1
 
 program = Program("Masters in Applied Computer Science", "MACS")
 
-student = Student("john.oliver", "password", "+3245678910", "student@uni.be", "John", "Oliver", "Gates", "STU1")
-student.address = address2
-student.program = program
+student1 = Student("jeff.bezos", "password1", "+3245678910", "jeff.bezos@uni.be", "Jeff", "Oliver", "Bezos", "STU1")
+student1.address = address2
+student1.program = program
 
-university = University("ua_uni1", "password", "+32889966771", "admin@uni.be", "UA", "UNI1")
+student2 = Student("bill.gates", "password2", "+3244557766", "bill.gates@uni.be", "Bill", "John", "Gates", "STU2")
+student2.address = address4
+student2.program = program
+
+university = University("ua_uni1", "password3", "+32889966771", "admin@uni.be", "UA", "UNI1")
 university.address = address3
 university.programs.append(program)
 
@@ -31,12 +36,13 @@ db.session.add(address1)
 db.session.add(address2)
 db.session.add(company)
 db.session.add(program)
-db.session.add(student)
+db.session.add(student1)
+db.session.add(student2)
 db.session.add(university)
 db.session.commit()
 
 db_student = db.session.query(Student).filter(Student.id == 2).one()
-db_university = db.session.query(University).filter(University.id == 3).one()
+db_university = db.session.query(University).filter(University.id == 4).one()
 db_company = db.session.query(Company).filter(Company.id == 1).one()
 internship = Internship("INT1", db_university.id, db_student.id, db_company.id, "AVAILABLE", datetime.today(),
                         datetime.today())
