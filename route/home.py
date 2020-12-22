ce40b11a0edf3363bf357ce62ddb26e8e5734de0
sincy <sincyraj86@gmail.com>
2020-12-22 23:08:04 +0100

    Implemented the update status functionality

--- route/home.py
+++ route/home.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, url_for, request, session, logging, re
 
 from sqlalchemy import create_engine, select
 from sqlalchemy.orm import scoped_session, sessionmaker
+from wtforms import form
+
 from model.address import Address
 from model.student import Student
 from model.user import User
@@ -33,15 +35,17 @@ def get_students():
     return render_template("students.html", students=students)
 
 
-@app.route("/internships", methods=["GET"])
-def get_internships():
+@app.route("/internships", methods=["GET", "POST"])
+def handle_internships():
     internships = internship_service.fetch_all_internships()
+
+    if request.method == "POST":
+        action = request.form.get("decision")
+        internship_ids = request.form.getlist("updateStatus")
+        internship_service.update_internships(action, internship_ids)
+        return redirect(url_for("handle_internships"))
     return render_template("internships.html", internships=internships)
 
-
-@app.route("/internships/change/status", methods=["POST"])
-def post_internship_status():
-    return "Done"
 
 
 if __name__ == "__main__":
--- service/internship_service.py
+++ service/internship_service.py
@@ -1,3 +1,6 @@
+from sqlalchemy import update
+
+from model.base import db
 from model.company import Company
 from model.internship import Internship
 from model.student import Student
@@ -9,3 +12,13 @@ def fetch_all_internships():
         .join(University, Internship.university_id == University.id)\
         .join(Student, Internship.student_id == Student.id)\
         .join(Company, Internship.company_id == Company.id).all()
+
+
+def update_internships(action, ids):
+    for id in ids:
+        if action == "approve":
+            status = "Approved"
+        elif action == "reject":
+            status = "Rejected"
+        db.session.execute(update(Internship).where(Internship.id == id).values(status=status))
+        db.session.commit()
