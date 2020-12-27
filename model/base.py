c9397908977f21f859756aa4bd8e66820207c827
sincyraj <sincyraj86@gmail.com>
2020-12-27 17:58:01 +0100

    Fixing errors

--- model/base.py
+++ model/base.py
@@ -2,5 +2,5 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, template_folder="../templates",static_folder="../static")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@127.0.0.1/test'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://vub:vub1234!@127.0.0.1/vub'
 db = SQLAlchemy(app)
